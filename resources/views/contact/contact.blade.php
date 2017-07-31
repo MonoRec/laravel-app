360cd716a2163a92245e1f7f7041ffbfa6a75fc0
MonoRec <batryxaaa@gmail.com>
2017-07-31 15:44:01 +0300

    30 lesson;

---
+++ resources/views/contact/contact.blade.php
@@ -0,0 +1,1 @@
+<a href=route('admin.home')> click me</a>;
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post/{id}/{name}','Posts\PostController@index');
+
+Route::resource('Posts', 'Posts\PostController');
